f5e849944c017206ad352288381915ab7e52e030
DanioFiore <daniofiore@hotmail.com>
2022-09-01 12:18:48 +0200

    completed the insert article form

--- app/Http/Livewire/CreateArticle.php
+++ app/Http/Livewire/CreateArticle.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Article;
 use Livewire\Component;
 
 class CreateArticle extends Component
@@ -9,13 +10,37 @@ class CreateArticle extends Component
     public $title;
     public $body;
 
+    protected $rules = [
+        'title' => 'required | min: 3',
+        'body' => 'required | min: 8',
+    ];
+
+    protected $messages = [
+        'required' => 'The field :attribute is required',
+        'min' => 'The field :attribute is too short',
+    ];
+
     public function store()
     {
         Article::create([
             'title'=>$this->title,
             'body'=>$this->body,
         ]);
+        session()->flash('message', 'Article successfully created!');
+        $this->clearForm();
     }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function clearForm()
+    {
+        $this->title = '';
+        $this->body = '';
+    }
+
     public function render()
     {
         return view('livewire.create-article');
--- resources/views/livewire/create-article.blade.php
+++ resources/views/livewire/create-article.blade.php
@@ -3,17 +3,35 @@
         Create an Article, dear Writer.
     </h2>
 
+    @if(session()->has('message'))
+        <div class="flex flex-row justify-center my-2 alert alert-success">
+            {{session('message')}}
+        </div>
+    @endif
+
     <form wire:submit.prevent="store">
         @csrf
 
         <div class="mb-3">
             <label class="text-warning" for="title">Title</label>
-            <input wire:model="title" type="text" class="form-control">
+            <input wire:model="title" type="text" class="form-control @error('title') is-invalid @enderror">
+            <div class="text-danger">
+                @error('title')
+                    {{$message}}
+                @enderror
+            </div>
+            
         </div>
 
         <div class="mb-3">
             <label class="text-warning" for="body">Article body</label>
-            <textarea wire:model="body" type="text" class="form-control"></textarea>
+            <textarea wire:model="body" type="text" class="form-control @error('body') is-invalid @enderror"></textarea>
+            <div class="text-danger">
+                @error('body')
+                    {{$message}}
+                @enderror
+            </div>
+            
         </div>
 
         <button type="submit" class="btn btn-outline-success">Create</button>
